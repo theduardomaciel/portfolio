3c36b044400982b08798b29182c52facf3b97d3a
Eduardo <eduardomacielbr@gmail.com>
2023-01-19 19:28:09 -0300

    Returning prisma client

--- src/lib/prismaClient.ts
+++ src/lib/prismaClient.ts
@@ -1,4 +1,4 @@
-/* import { PrismaClient } from '@prisma/client/edge'
+import { PrismaClient } from '@prisma/client/edge'
 
 declare global {
     // allow global `var` declarations
@@ -17,27 +17,5 @@ const prisma =
     }) // { log: ['query']}
 
 if (import.meta.env.NODE_ENV !== "production") global.prisma = prisma
-
-export default prisma; */
-
-import type { PrismaClient as PrismaClientType } from '@prisma/client/index';
-import { PrismaClient } from '@prisma/client/edge';
-
-let prisma: PrismaClientType | undefined;
-
-if (import.meta.env.MODE === 'development') {
-    prisma = new PrismaClient();
-} else {
-    import('@prisma/client/edge').then(
-        mod =>
-        (prisma = new mod.PrismaClient({
-            datasources: {
-                db: {
-                    url: import.meta.env.DATABASE_URL,
-                },
-            },
-        }))
-    );
-}
 
 export default prisma;
