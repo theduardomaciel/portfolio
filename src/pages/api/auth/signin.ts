cd8dfcecc9f3933b1deb05fb7ec0b78717ed7c42
Eduardo <eduardomacielbr@gmail.com>
2023-06-01 00:45:51 -0300

    Font fixes

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -6,7 +6,7 @@ import prisma from "src/lib/prisma";
 
 const GITHUB_ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token";
 const { PUBLIC_GITHUB_REDIRECT_URI, PUBLIC_GITHUB_CLIENT_ID, GITHUB_SECRET } =
-	process.env;
+	import.meta.env;
 
 async function getAccessTokenFromCode(code: string, state: string) {
 	const params = {
