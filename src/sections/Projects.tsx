e7ce64bfccd5a45bb86dd8aa1a1c3fa1c7383da3
Eduardo <eduardomacielbr@gmail.com>
2023-09-06 00:58:23 -0300

    Homepage updates!

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -16,177 +16,177 @@ import type Project from "src/lib/types/project";
 import { useTranslations } from "src/i18n/utils";
 
 type ColumnProps = {
-	name: string;
-	link?: string;
-	techs: Array<string>;
-	outro?: string;
+    name: string;
+    link?: string;
+    techs: Array<string>;
+    outro?: string;
 };
 
 interface Props {
-	projects: Project[];
-	lang?: string;
+    projects: Project[];
+    lang?: string;
 }
 
 import type { ui } from "src/i18n/ui";
 
 export default function Projects({ projects, lang }: Props) {
-	const [projectIndex, setProjectIndex] = useState(0);
-	const [isMoreInfoExpanded, setMoreInfoExpanded] = useState(true);
+    const [projectIndex, setProjectIndex] = useState(0);
+    const [isMoreInfoExpanded, setMoreInfoExpanded] = useState(true);
 
-	const t = useTranslations(lang as keyof typeof ui);
+    const t = useTranslations(lang as keyof typeof ui);
 
-	return (
-		<section className={`${styles.projects} section wrapper`} id="projects">
-			<header className={"subtitle"}>
-				<p>{t("projects.title")}</p>
-				<div />
-			</header>
+    return (
+        <section className={`${styles.projects} section wrapper`} id="projects">
+            <header className={"subtitle"}>
+                <p>{t("projects.title")}</p>
+                <div />
+            </header>
 
-			<LazyMotion features={domAnimation}>
-				<ProjectsCarousel
-					lang={lang}
-					projects={projects as Project[]}
-					projectIndex={projectIndex}
-					setMoreInfoExpanded={setMoreInfoExpanded}
-					isMoreInfoExpanded={isMoreInfoExpanded}
-				/>
-			</LazyMotion>
+            <LazyMotion features={domAnimation}>
+                <ProjectsCarousel
+                    lang={lang}
+                    projects={projects as Project[]}
+                    projectIndex={projectIndex}
+                    setMoreInfoExpanded={setMoreInfoExpanded}
+                    isMoreInfoExpanded={isMoreInfoExpanded}
+                />
+            </LazyMotion>
 
-			<div className={styles.sectionDots}>
-				<ChevronLeft
-					style={{
-						cursor: projectIndex === 0 ? "initial" : "pointer",
-						transition: "0.5s",
-					}}
-					fill={projectIndex === 0 ? "#808080" : "var(--primary-01)"}
-					onClick={() => {
-						if (projectIndex > 0) {
-							setProjectIndex(projectIndex - 1);
-						}
-					}}
-				/>
-				<ul>
-					{projects.map(function (project, index) {
-						const isCurrentProject = index === projectIndex;
-						return (
-							<li
-								onClick={() => setProjectIndex(index)}
-								key={index}
-							>
-								<div
-									style={{
-										backgroundColor: isCurrentProject
-											? `rgb(${project.accent_color})`
-											: "var(--primary-01)",
-									}}
-									className={`${
-										isCurrentProject && "bulletUp"
-									}`}
-								/>
-							</li>
-						);
-					})}
-				</ul>
-				<ChevronLeft
-					style={{
-						cursor:
-							projectIndex === projects.length - 1
-								? "initial"
-								: "pointer",
-						transition: "0.5s",
-						transform: "rotate(180deg)",
-					}}
-					fill={
-						projectIndex === projects.length - 1
-							? "#808080"
-							: "var(--primary-01)"
-					}
-					onClick={() => {
-						if (projectIndex < projects.length - 1) {
-							setProjectIndex(projectIndex + 1);
-						}
-					}}
-				/>
-			</div>
-			{projects[projectIndex].technologies.length >= 1 && (
-				<div
-					className={styles.projectTechnologies}
-					style={{
-						maxHeight: isMoreInfoExpanded ? "50rem" : "0",
-					}}
-				>
-					{projects[projectIndex].technologies.length > 1 && (
-						<h5>{t("projects.info.title")}</h5>
-					)}
-					<div className={styles.projectTechs}>
-						<ul className={styles.projectTechs}>
-							{projects[projectIndex].technologies.map(
-								(column: ColumnProps, index) => (
-									<li key={index}>
-										<div className={styles.column}>
-											<a
-												target={"_blank"}
-												rel="noreferrer"
-												href={column.link}
-												className={styles.title}
-											>
-												<h6
-													className={
-														column.hasOwnProperty(
-															"link"
-														)
-															? styles.link
-															: ""
-													}
-												>
-													{column.name}
-												</h6>
-												{column.hasOwnProperty(
-													"link"
-												) && <LinkIcon />}
-											</a>
-											<ul key={`${index}_tech`}>
-												{column.techs.map(
-													(tech, index) => (
-														<li key={index}>
-															{tech}
-														</li>
-													)
-												)}
-											</ul>
-											{column.hasOwnProperty("outro") && (
-												<p>{column.outro}</p>
-											)}
-										</div>
-									</li>
-								)
-							)}
-						</ul>
-					</div>
-				</div>
-			)}
-			<a
-				target={"_blank"}
-				rel="noreferrer"
-				href="https://github.com/theduardomaciel"
-			>
-				<button
-					className="button inverted modern"
-					style={{
-						fontSize: `1.4rem`,
-						paddingInline: `3rem`,
-						paddingBlock: "1.25rem",
-						marginBottom: "1rem",
-					}}
-				>
-					<GithubLogo
-						width={`1.8rem`}
-						height={`1.8rem`}
-						aria-label="Github icon"
-					/>
-					{t("projects.info.github")}
-				</button>
-			</a>
-		</section>
-	);
+            <div className={styles.sectionDots}>
+                <ChevronLeft
+                    style={{
+                        cursor: projectIndex === 0 ? "initial" : "pointer",
+                        transition: "0.5s",
+                    }}
+                    fill={projectIndex === 0 ? "#808080" : "var(--primary-01)"}
+                    onClick={() => {
+                        if (projectIndex > 0) {
+                            setProjectIndex(projectIndex - 1);
+                        }
+                    }}
+                />
+                <ul>
+                    {projects.map(function (project, index) {
+                        const isCurrentProject = index === projectIndex;
+                        return (
+                            <li
+                                onClick={() => setProjectIndex(index)}
+                                key={index}
+                            >
+                                <div
+                                    style={{
+                                        backgroundColor: isCurrentProject
+                                            ? `rgb(${project.accent_color})`
+                                            : "var(--primary-01)",
+                                    }}
+                                    className={`${
+                                        isCurrentProject && "bulletUp"
+                                    }`}
+                                />
+                            </li>
+                        );
+                    })}
+                </ul>
+                <ChevronLeft
+                    style={{
+                        cursor:
+                            projectIndex === projects.length - 1
+                                ? "initial"
+                                : "pointer",
+                        transition: "0.5s",
+                        transform: "rotate(180deg)",
+                    }}
+                    fill={
+                        projectIndex === projects.length - 1
+                            ? "#808080"
+                            : "var(--primary-01)"
+                    }
+                    onClick={() => {
+                        if (projectIndex < projects.length - 1) {
+                            setProjectIndex(projectIndex + 1);
+                        }
+                    }}
+                />
+            </div>
+            {projects[projectIndex].technologies.length >= 1 && (
+                <div
+                    className={styles.projectTechnologies}
+                    style={{
+                        maxHeight: isMoreInfoExpanded ? "100rem" : "0",
+                    }}
+                >
+                    {projects[projectIndex].technologies.length > 1 && (
+                        <h5>{t("projects.info.title")}</h5>
+                    )}
+                    <div className={styles.projectTechs}>
+                        <ul className={styles.projectTechs}>
+                            {projects[projectIndex].technologies.map(
+                                (column: ColumnProps, index) => (
+                                    <li key={index}>
+                                        <div className={styles.column}>
+                                            <a
+                                                target={"_blank"}
+                                                rel="noreferrer"
+                                                href={column.link}
+                                                className={styles.title}
+                                            >
+                                                <h6
+                                                    className={
+                                                        column.hasOwnProperty(
+                                                            "link"
+                                                        )
+                                                            ? styles.link
+                                                            : ""
+                                                    }
+                                                >
+                                                    {column.name}
+                                                </h6>
+                                                {column.hasOwnProperty(
+                                                    "link"
+                                                ) && <LinkIcon />}
+                                            </a>
+                                            <ul key={`${index}_tech`}>
+                                                {column.techs.map(
+                                                    (tech, index) => (
+                                                        <li key={index}>
+                                                            {tech}
+                                                        </li>
+                                                    )
+                                                )}
+                                            </ul>
+                                            {column.hasOwnProperty("outro") && (
+                                                <p>{column.outro}</p>
+                                            )}
+                                        </div>
+                                    </li>
+                                )
+                            )}
+                        </ul>
+                    </div>
+                </div>
+            )}
+            <a
+                target={"_blank"}
+                rel="noreferrer"
+                href="https://github.com/theduardomaciel"
+            >
+                <button
+                    className="button inverted modern"
+                    style={{
+                        fontSize: `1.4rem`,
+                        paddingInline: `3rem`,
+                        paddingBlock: "1.25rem",
+                        marginBottom: "1rem",
+                    }}
+                >
+                    <GithubLogo
+                        width={`1.8rem`}
+                        height={`1.8rem`}
+                        aria-label="Github icon"
+                    />
+                    {t("projects.info.github")}
+                </button>
+            </a>
+        </section>
+    );
 }
